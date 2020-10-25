ce1bbe006337a4a8d223406d8410f6dcfe6a8b45
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 16:43:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// prewritten
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
